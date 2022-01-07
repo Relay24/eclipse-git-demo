b3ca25daaa16aeb37aa491727abcd12c6bd2ff67
Yury Berahenka <mycantora@gmail.com>
2022-01-07 16:30:01 +0300

    second commit to test branch

--- src/by/example/Runner.java
+++ src/by/example/Runner.java
@@ -6,6 +6,7 @@ public class Runner {
     System.out.println("Hello world!");
     System.out.println("Hello world!");
     System.out.println("Hello world!");
+    System.out.println("Hello world!");
   }
 
 }
