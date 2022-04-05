32072e169fe863e1e0b95ddc843ca5aeac76b18c
Yury Berahenka <mycantora@gmail.com>
2022-04-06 00:03:13 +0300

    test commit after update

--- src/by/example/Runner.java
+++ src/by/example/Runner.java
@@ -7,6 +7,7 @@ public class Runner {
     System.out.println("Hello world!");
     System.out.println("Hello world!");
     System.out.println("Hello world!");
+    System.out.println("Hello world!");
     name();
   }
   
